18af6025e62b5d5300ea0f9c3873f6a0547a4415
Robert Ahlin <rob@ahlin.org>
2024-03-07 20:46:13 +0100

    Added news section on index page

---
+++ components/NewsSection.js
@@ -0,0 +1,24 @@
+// components/NewsSection.js
+
+import React from 'react';
+import newsData from '../db/news.json';
+import styles from './NewsSection.module.css';
+
+const NewsSection = () => {
+  const latestNews = newsData.Nyheter.slice(0, 3);
+
+  return (
+    <div className={styles.newsSection}>
+      <h2>Latest News</h2>
+      {latestNews.map((newsItem, index) => (
+        <div key={index} className={styles.newsItem}>
+          <h3>{newsItem.title}</h3>
+          <p>{newsItem.date}</p>
+          <p>{newsItem.text}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default NewsSection;
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Header from '../components/Header';
 import HeroSection from '../components/HeroSection'; // Import the HeroSection component
 import Footer from '../components/Footer';
 import styles from '../styles/Home.module.css'; // Adjust this according to your styles
+import NewsSection from '../components/NewsSection';
 
 const Home = () => {
   return (
@@ -15,6 +16,7 @@ const Home = () => {
         title="Welcome to the e-learning platform!" 
         subtitle="Learn anytime, anywhere."
       />
+      <NewsSection style={{ marginBottom: '120px' }} />
       <Footer />
     </div>
   );
